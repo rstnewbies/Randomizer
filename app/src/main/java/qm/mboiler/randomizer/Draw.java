2efaba8188341e858a840dcdc88e692ef9a1952c
Dekad300 <bakowskiradek1@gmail.com>
2017-06-14 11:56:40 +0100

    sharedpreferences edit #1
    zmiana getInt na putInt

--- app/src/main/java/qm/mboiler/randomizer/Draw.java
+++ app/src/main/java/qm/mboiler/randomizer/Draw.java
@@ -14,7 +14,7 @@ public class Draw {
             // MY_PREFS_NAME - a static String variable like:
             //public static final String MY_PREFS_NAME = "MyPrefsFile";
             SharedPreferences.Editor editor = getSharedPreferences(MY_PREFS_NAME, MODE_PRIVATE).edit();
-            editor.putInt("idName", 6);
+            editor.getInt("idName", 6);
             int variable = 6;
             Random rand = new Random();
             int number = rand.nextInt(variable)+1;
